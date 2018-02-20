61ba37aa1433852b940fe4233bd08669ef1c428b
wldms6742 <36650354+wldms6742@users.noreply.github.com>
2018-02-20 10:23:46 +0900

    Test1

--- src/com/iu/s1/Main.java
+++ src/com/iu/s1/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		boolean check=true;
-		while(check) {
+
 			System.out.println("번호를 입력하세요");
 			int num = sc.nextInt();
 			SchoolController school_c= new SchoolController();
 			school_c.start(num);
-			}
+			System.out.println("program start");
+
 	}
 
 }
